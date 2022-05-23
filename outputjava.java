975a55bd5c88515dd10a67d3a9e48d04810637ba
sachinsadanandan <103180738+sachinsadanandan@users.noreply.github.com>
2022-05-23 14:45:15 +0530

    Add files via upload

---
+++ outputjava.java
@@ -0,0 +1,22 @@
+Enter the number of rows:3
+Enter the number of columns:3
+Enter the matrix elements:
+1 2 3
+4 5 6
+7 8 9
+The matrix formed is:
+1       2       3
+4       5       6
+7       8       9
+
+The matrix is not symmetric
+D:\SACHINJAVA>javac JavaExample.java
+
+D:\SACHINJAVA>java JavaExample.java
+Enter number of strings you would like to enter:3
+Enter the Strings one by one:
+sachin
+sijo
+jeffin
+Strings in Sorted Order:jeffin, sachin, sijo,
+D:\SACHINJAVA>
